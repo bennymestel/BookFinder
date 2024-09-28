05a380c69fb969d37e81c3f0dbeec7694117e2ac
bennymestel <56840166+bennymestel@users.noreply.github.com>
2024-09-29 01:20:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,6 @@ import numpy as np
 # Set page configuration
 st.set_page_config(page_title="BookFinder", page_icon="📚")
 
-# Include Google Analytics tracking code
-with open("google_analytics.html", "r") as f:
-    html_code = f.read()
-    components.html(html_code, height=0)
-
 # Load the CSV file
 @st.cache_resource
 def load_data():
